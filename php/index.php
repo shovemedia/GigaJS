ddb46768237b877462cf7ceec8682e8bff131103
Jon Williams <jon@shovemedia.com>
2013-10-18 22:55:26 -0400

    tag error

--- php/index.php
+++ php/index.php
@@ -7,43 +7,29 @@
 	<head>
 		<title>Giga Demo Site</title>
 
-	<script src="js/lib/require.js"></script>
-	<script src="js/lib/require.config.js"></script>
-	
-	
-	<script>
-	
-	require(
-		[
-			"jquery"
-		]
-		,	  			
-		function ($) {	
-			$(function ()
-			{		
-				require(['test/Site'], function (Main) {
-					var main = new Main($('#contextView'));
+		<script src="js/lib/require.js"></script>
+		<script src="js/lib/require.config.js"></script>
+		
+		
+		<script>		
+		require(
+			[
+				"jquery"
+			]
+			,	  			
+			function ($) {	
+				$(function ()
+				{		
+					require(['test/Site'], function (Main) {
+						var main = new Main($('#contextView'));
 
-					// 
-					$('body').css('display', 'block');
+						// 
+						$('body').css('display', 'block');
+					});
 				});
-			});
-  	});	
-	</script>
-</head>
-<body>
-
-
-
-
-
-
-
-
+	  	});	
+		</script>
 	</head>
-
-
-
 	<body id="contextView">
 		<script type="text/javascript">
 			// if JS supported, hide everything
